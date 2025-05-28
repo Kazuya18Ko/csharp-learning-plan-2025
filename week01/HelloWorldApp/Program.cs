0f7f15aa581b7adf1dfa9b8e5796338f8bb81793
Kazuya Kojima <kazuya_kojima_15@yahoo.co.jp>
2025-05-28 20:14:42 +0900

    利用するsdkが9.0.300になっていたため、grobal.jsonによって8.0.410を利用するよう変更

---
+++ week01/HelloWorldApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Print "Hello, World!" to the console
+        Console.WriteLine("Hello, World!");
+        
+        // Wait for user input before closing the console window
+        Console.ReadLine();
+    }
+}
