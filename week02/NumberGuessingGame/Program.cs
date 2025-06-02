067b46d78cff2d5ec1dd5e89f48ee141d03715ff
Kazuya Kojima <kazuya_kojima_15@yahoo.co.jp>
2025-06-02 18:02:18 +0900

    feat:Program.csに正解判定とループを追加(#16)

--- week02/NumberGuessingGame/Program.cs
+++ week02/NumberGuessingGame/Program.cs
@@ -1,9 +1,7 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
+class Program {
+    static void Main(string[] args) {
         // 1.乱数の生成
         // Random変数を生成するためのインスタンスを作成
         Random rnd = new Random();
@@ -17,11 +15,9 @@ class Program
 
         // 2.ユーザー入力の取得
 
-        while (true)
-        {
+        while (true) {
             Console.WriteLine($"1～100までの数字を入力してください");
             string input = Console.ReadLine();
-            attempts++;
 
             // 入力値の検証
             if (!int.TryParse(input, out guessNum)) {
@@ -29,15 +25,22 @@ class Program
                 continue;
             }
 
-            if (guessNum < 1 || guessNum > 100)
-            {
+            if (guessNum < 1 || guessNum > 100) {
                 Console.WriteLine($"1～100までの数字を入力してください。入力された数字:({guessNum})\n");
                 continue;
-
             }
-            else
-            {
-                Console.WriteLine($"入力された数字:{guessNum}, 試行回数:{attempts}");
+
+            if (guessNum < correctNum) {
+                attempts++; //試行回数のカウント
+                Console.WriteLine($"{attempts}:もっと大きい数字です。\n");
+            }
+            else if (guessNum > correctNum) {
+                attempts++; //試行回数のカウント
+                Console.WriteLine($"{attempts}:もっと小さい数字です。\n");
+            }
+            else {
+                attempts++; //試行回数のカウント
+                Console.WriteLine($"正解です！\n試行回数:{attempts}");
                 break;
             }
         }
