7080f653cd53abdb6096bb5663103b170f20bc0d
Kazuya Kojima <kazuya_kojima_15@yahoo.co.jp>
2025-06-02 16:47:12 +0900

    feat:Program.csに乱数生成部分を追加(#16)

--- week02/NumberGuessingGame/Program.cs
+++ week02/NumberGuessingGame/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Random変数を生成するためのインスタンスを作成
+        Random rnd = new Random();
+        //1から100までのランダムな整数を生成
+        int correctNum = rnd.Next(0, 101);
+
+        int guessNum = -1; //ユーザー入力の初期値を設定
+        int attempts = 0; //試行回数の初期値を設定
+
+        // Console.WriteLine($"debug:correctNum={correctNum}");
+    }
+}
