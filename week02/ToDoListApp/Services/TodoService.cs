e6aa516dafd3491f15243666806e5d19ba7614e2
Kazuya Kojima <kazuya_kojima_15@yahoo.co.jp>
2025-06-05 13:49:53 +0900

    feat:前回作成したServices/TodoService.csの文法を大きく間違えていたため一旦修正。(#21)

--- week02/ToDoListApp/Services/TodoService.cs
+++ week02/ToDoListApp/Services/TodoService.cs
@@ -1,9 +1,21 @@
-using Models;
+using ToDoListApp.Models;
 
 public class Todoservice
 {
-    public void Add(string title) { }
-    public List<TodoItem> GetAll() -> new();
-    public bool Complete(int id) -> false;
-    public bool Delete(int id) -> false;
+    public void Add(string title)
+    {
+        
+    }
+    public List<TodoItem> GetAll()
+    {
+        return new();
+    }
+    public bool Complete(int id)
+    {
+        return false;
+    }
+    public bool Delete(int id)
+    {
+        return false
+    }
 }
