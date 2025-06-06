cf4a1c3d04d473de572214bb851199a651f5a570
Kazuya Kojima <kazuya_kojima_15@yahoo.co.jp>
2025-06-06 18:02:15 +0900

    feat:Program.csにユーザーの選択に応じたswitch-case分岐を仮実装。(#34)

--- week02/ToDoListApp/Program.cs
+++ week02/ToDoListApp/Program.cs
@@ -9,5 +9,58 @@ class Program {
 
         // リストの初回読み込み
         var service = new TodoService();
+        int n = 0;
+
+        while(true)
+        {
+            // コンソール表示処理
+            // この部分は後にConsoleView.csに切り分ける
+            Console.WriteLine("=== Todoリスト アプリ ===");
+            Console.WriteLine("1. タスクを追加");
+            Console.WriteLine("2. タスクを一覧表示");
+            Console.WriteLine("3. タスクを完了");
+            Console.WriteLine("4. タスクを削除");
+            Console.WriteLine("5. 終了");
+            Console.Write("番号を選んでください: ");
+
+            string? input = Console.ReadLine();
+
+            if(!int.TryParse(input,out n))
+            {
+                Console.WriteLine("数字を入力してください");
+                Console.WriteLine(""); //改行
+                continue;
+            }
+            if(n <= 0 ||  n > 5)
+            {
+                Console.WriteLine("1～5の数字を入力してください");
+                Console.WriteLine(""); //改行
+                continue;
+            }
+            else
+            {
+                // ここがProgram.csの本当の処理領域
+                switch(n)
+                {
+                    case 1:
+                        Console.WriteLine($"{n}:が入力されました"); //デバッグ
+                        break;
+                    case 2:
+                        Console.WriteLine($"{n}:が入力されました"); //デバッグ
+                        break;
+                    case 3:
+                        Console.WriteLine($"{n}:が入力されました"); //デバッグ
+                        break;
+                    case 4:
+                        Console.WriteLine($"{n}:が入力されました"); //デバッグ
+                        break;
+                    case 5:
+                        Console.WriteLine($"{n}:が入力されました"); //デバッグ
+                        Console.WriteLine("プログラムを終了します"); //終了処理
+                        return;
+                }
+            }
+            Console.WriteLine(""); //改行
+        }
     }
 }
